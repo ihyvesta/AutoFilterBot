4b651371434fe26c988a4662ba7736a98394dcee
ihyvesta <160210388+ihyvesta@users.noreply.github.com>
2024-06-19 07:40:09 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Hislordshipb/Hislordshipb.git /DQ
+  git clone https://github.com/ihyvesta/AutoFilterBot.git /DQ
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Hislordshipb
+  git clone $UPSTREAM_REPO /AutoFilterBot
 fi
-cd /Hislordshipb
+cd /AutoFilterBot
 pip3 install -U -r requirements.txt
-echo "Starting Hislordshipb...."
+echo "Starting ...."
 python3 bot.py
